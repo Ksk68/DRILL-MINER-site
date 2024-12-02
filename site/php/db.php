7b02ebb4c7b4a4b8bae1a52b682f4341bb9862d2
Raul Viela <raulvilela16@gmail.com>
2024-12-02 20:45:56 +0000

    a
    a

--- site/php/db.php
+++ site/php/db.php
@@ -1,5 +1,4 @@
 <?php   
-    $codigo_jogo = 0;
     function conexao(){
         $server_name = "127.0.0.1";
         $username = "root";
@@ -13,30 +12,27 @@
         }
         return $conexao;
     }
-    
-    // vereficar o codigo do user
-    function vereficar_codigo ($codigo){
-        global $codigo_jogo;
-        $query_codigo = "SELECT * FROM codigo_jogo  WHERE codigo LIKE '$codigo'";
-        $resultado = mysqli_query(conexao(), $query_codigo);
-        
-        if ($resultado && mysqli_num_rows($resultado) == 1) { //verificar se a tabela tem mais de 0 row
-            $codigo_jogo = $codigo;
-            return "deu";
-        } else {
-            return "erro";
-        }
-    }
 
     //verificar o login
-    function verificar_login ($user, $pass){ 
-        echo $user, $pass;
-        $query_codigo = "SELECT  FROM user WHERE = '$user' AND  =    ";
-        $resultado = mysqli_query(conexao(), $query_codigo);
-        if (mysqli_num_rows($resultado) == 1) { //verificar se a tabela tem 1 row
-            
+    function verificar_login ($email, $pass){ 
+
+        if (empty($email) || empty($pass)) {
+            return "nada";
+        }
+
+        $stmt = conexao()->prepare("SELECT password FROM user WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if (password_verify($pass, $row['password'])) {
+                return "login";
+            } else {
+                return "senha";
+            }
         } else {
-            
+            return "email";
         }
     }
 
